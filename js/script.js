a4558e83035f6d11704b014160eeee15cf5c68db
MaxIvaniuk <meteorit614@gmail.com>
2021-04-06 23:57:07 +0300

    debug and done form resetting, done submitting and calculating results, add favicon, clear code

--- js/script.js
+++ js/script.js
@@ -1,35 +1,5 @@
 'use strict'
 
-// Формулы
-// Поддержание веса
-    
-// Для женщин:
-    
-// N = (10 × вес в килограммах) + (6,25 × рост в сантиметрах) − (5 × возраст в годах) − 161
-    
-// Для мужчин:
-    
-// N = (10 × вес в килограммах) + (6,25 × рост в сантиметрах) − (5 × возраст в годах) + 5
-    
-// Полученное значение (N) умножаем на коэффициент активности. Результат и будет нормой калорий для поддержания веса.
-// Коэффициенты активности
-    
-//     Минимальная: 1.2.
-//     Низкая: 1.375.
-//     Средняя: 1.55.
-//     Высокая: 1.725.
-//     Очень высокая: 1.9.
-    
-// Формулы для набора и сброса веса
-    
-//     Набор веса: прибавляем 15% от нормы к этой норме.
-//     Сброс веса: вычитаем 15% от нормы из этой нормы.
-
-// perfectWeight = (10 * weight) + (6.25 * height) - (5 * age) - genderIndex;
-// calorieNorm = perfectWeight * activityRate;
-// weightGain = calorieNorm + (calorieNorm * 15 / 100);
-// weightLoss = calorieNorm - (calorieNorm * 15 / 100);
-
 const activityCoef = {
     min: 1.2,
     low: 1.375,
@@ -40,11 +10,10 @@ const activityCoef = {
 
 const genderSwitch = document.querySelector('.switcher'),
       humanParameters = document.querySelector('.inputs-group'),
-      radioBtns = document.querySelector('.radios-group'),
+      radiosContainer = document.querySelector('.radios-group'),
       submitBtn = document.querySelector('.form__submit-button'),
       resetBtn = document.querySelector('.form__reset-button'),
       formSubmit = document.querySelector('.form__submit'),
-      form = document.querySelector('#form'),
       counterResult = document.querySelector('.counter__result');
  
 let age = 0,
@@ -55,7 +24,14 @@ let age = 0,
     radioValue = '',
     activityRate = activityCoef.min,
     perfectWeight,
-    radioBtnValue = document.querySelector('input[name="activity"]:checked');
+    radioBtns = document.getElementsByName('activity'),
+    maintainingWeightResult = document.querySelector('#calories-norm'),
+    weightLossResult = document.querySelector('#calories-minimal'),
+    weightGainResult = document.querySelector('#calories-maximal'),
+    maintainingWeight,
+    calorieNorm = 0,
+    weightGain = 0,
+    weightLoss = 0;
 
 // Get users gender and set gender index
 function switcherTarget(event) {
@@ -84,7 +60,7 @@ function personParams(event){
             weight = input.value;
             break;
         default:
-            console.log(`Пожалуйста введите данные`);
+            alert(`Пожалуйста введите данные`);
     }
 }
 
@@ -92,54 +68,69 @@ humanParameters.addEventListener('change', personParams);
 
 // Get users activity rate 
 function activityChoice(){
-    switch(radioBtnValue.value) {
-        case 'low': 
-            activityRate = activityCoef.low;
-            radioValue = '';
-            break;
-        case 'medium':
-            activityRate = activityCoef.med;
-            radioValue = '';
-            break;
-        case 'high':
-            activityRate = activityCoef.high;
-            radioValue = '';
-            break;
-        case 'max':
-            activityRate = activityCoef.max;
-            radioValue = '';
-            break;
-        default:
-            activityRate = activityCoef.min;
+    for(let i = 0; i < radioBtns.length; i++){
+        if(radioBtns[i].checked == true){
+            switch(radioBtns[i].value) {
+                case 'low': 
+                    activityRate = activityCoef.low;
+                    radioValue = '';
+                    break;
+                case 'medium':
+                    activityRate = activityCoef.med;
+                    radioValue = '';
+                    break;
+                case 'high':
+                    activityRate = activityCoef.high;
+                    radioValue = '';
+                    break;
+                case 'max':
+                    activityRate = activityCoef.max;
+                    radioValue = '';
+                    break;
+                default:
+                    activityRate = activityCoef.min;
+            }
+        }
     }
-    console.log(activityRate);
 }
 
-radioBtns.addEventListener('click', activityChoice);
+radiosContainer.addEventListener('change', activityChoice);
 
 // Set buttons 'enable'
 function enableBtn(){
     if(genderIndex != 0 && activityRate != 0 && age != 0 && height != 0 && weight != 0) {
         submitBtn.removeAttribute('disabled');
         resetBtn.removeAttribute('disabled');
-        console.log('кнопка активна');
     } else {
         submitBtn.setAttribute('disabled', 'disabled');
         resetBtn.setAttribute('disabled', 'disabled');
-        console.log('кнопка dont активна');
     }
-};
+}
 
-radioBtns.addEventListener('click', enableBtn);
+radiosContainer.addEventListener('change', enableBtn);
 humanParameters.addEventListener('keyup', enableBtn);
 
-function resetForm(){
+// Form resetting
+function resetForm(event){
+    event.target.parentElement.form.reset();
     submitBtn.setAttribute('disabled', 'disabled');
     resetBtn.setAttribute('disabled', 'disabled');
     counterResult.classList.add('counter__result--hidden');
-    // form.reset();
 }
+
 resetBtn.addEventListener('click', resetForm);
+
+// Calculate and show results
+function calculateResults(){
+    maintainingWeight = (10 * weight) + (6.25 * height) - (5 * age) - (genderIndex);
+    calorieNorm = maintainingWeight * activityRate;
+    weightGain = calorieNorm + (calorieNorm * 15 / 100);
+    weightLoss = calorieNorm - (calorieNorm * 15 / 100);
+
+    maintainingWeightResult.textContent = Math.round(calorieNorm);
+    weightLossResult.textContent = Math.round(weightLoss);
+    weightGainResult.textContent = Math.round(weightGain);
+}
 
 // Form submitting
 function onSubmit(event) {
@@ -153,6 +144,7 @@ function onSubmit(event) {
         if (xhr.status === 200) {
             if(submitBtn.hasAttribute('disabled') == false){
                 counterResult.classList.remove('counter__result--hidden');
+                calculateResults();
             }
         } else {
             alert('Something went wrong please try again');
